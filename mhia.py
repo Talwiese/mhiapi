25e3c7326d712749ca514c9d85503768d64d1e5c
iman <iman@gmx.at>
2023-06-26 18:03:25 +0100

    add license inside .py files

--- mhia.py
+++ mhia.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
+
+# mhia.py - main process of the mhia pi aplication
+# Copyright (C) 2023  Iman Ayatollahi
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import os, subprocess, time, sys, logging, logging.config
 from modules.inhouse.signalhandler import SignalHandler
 from modules.inhouse.mhiacfg import MhiaConfig
